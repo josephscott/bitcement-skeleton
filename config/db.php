8c34e3c1fe25b9f5f1001883fa1dcf44fa046459
Joseph Scott <joseph@josephscott.org>
2021-02-18 21:23:57 -0700

    DB config example

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+declare( strict_types = 1 );
+
+/* MySQL Example
+$app->inject( 'db', adoNewConnection( 'mysqli' ) );
+$app->use( 'db' )->debug = true;
+$app->use( 'db' )->connect(
+	'127.0.0.1',
+	'username',
+	'password',
+	'db-name'
+);
+ */
