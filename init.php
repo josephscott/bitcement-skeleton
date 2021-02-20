e0fe33c8e2d82eb517c8e393b441d53b0c1b6dff
Joseph Scott <joseph@josephscott.org>
2021-02-19 23:26:23 -0700

    Fix typo

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 declare( strict_types = 1 );
 
 define( 'APP_DIR', __DIR__ );
-require APP_DIR . '/vender/autoload.php';
+require APP_DIR . '/vendor/autoload.php';
 
 $app = new BitCement\App();
 require APP_DIR . '/config/boot.php';
