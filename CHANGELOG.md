9f83c1a057bb09e201a8b46d327c9aa29d64066d
Joseph Scott <joseph@josephscott.org>
2021-02-20 10:56:08 -0700

    Only enable the Twig cache if the path is writable

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 000.000.002 - ????
+- Only enable the Twig cache if the directory is writable
--- config/twig.php
+++ config/twig.php
@@ -7,12 +7,20 @@ if ( !defined( 'APP_TWIG_AUTO_RELOAD' ) ) {
 	define( 'APP_TWIG_AUTO_RELOAD', true );
 }
 
+if ( !defined( 'APP_TWIG_CACHE_DIR' ) ) {
+	if ( is_writable( APP_DIR . '/cache/twig' ) ) {
+		define( 'APP_TWIG_CACHE_DIR', APP_DIR . '/cache/twig' );
+	} else {
+		define( 'APP_TWIG_CACHE_DIR', false );
+	}
+}
+
 $app->inject(
 	'twig',
 	new \Twig\Environment(
 		new \Twig\Loader\FilesystemLoader( APP_DIR . '/templates' ),
 		[
-			'cache' => APP_DIR . '/cache/twig',
+			'cache' => APP_TWIG_CACHE_DIR,
 			'auto_reload' => APP_TWIG_AUTO_RELOAD
 		]
 	)
